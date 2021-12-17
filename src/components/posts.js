0184834239bcf5d51b5e8b353554cb3cdf736af4
Benny Wong <12177862+BennyW23@users.noreply.github.com>
2021-12-17 13:53:28 -0800

    Added CSS and hooked up the frontend to the backend

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,34 +1,86 @@
 import React, { useEffect, useState } from "react";
 
+const BASE_URL = "https://worker.intern-assignment.workers.dev";
 const Posts = () => {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         const getPosts = async () => {
             const resp = await fetch(
-                "https://worker.intern-assignment.workers.dev/api/posts"
+                BASE_URL + "/api/posts"
             );
             const postsResp = await resp.json();
             setPosts(postsResp);
         };
 
         getPosts();
-    }, []);
+    }, [posts]);
 
     return (
-        <div>
-            <h1>Posts</h1>
+        <div className="container">
+            <h1>Blogs R Us</h1>
+            <NewPost posts={posts}/>
             {posts.map((post) => (
-                <div>
+                <div className="post-outline flex-item centered">
                     <h1>{post.title}</h1>
-                    <p>{post.text}</p>
                     <p>
                         <em>By {post.username}</em>
                     </p>
+                    <p>{post.content}</p>
                 </div>
             ))}
         </div>
     );
 };
 
+function NewPost(posts) {
+    const [title, setTitle] = useState("");
+    const [username, setUsername] = useState("");
+    const [content, setContent] = useState("");
+
+    const sharePost = async () => {
+        await fetch(BASE_URL + "/api/posts", {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'no-cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title: title, username: username, content: content}) // body data type must match "Content-Type" header
+        });
+        setTitle("");
+        setUsername("");
+        setContent("");
+
+    }
+    return (
+        <div className="post-outline flex-item">
+            <h1>
+            <input type="text"
+                   value={title}
+                   className="title-input"
+                   onChange={(event) => {setTitle(event.target.value)}}
+                   placeholder="Enter a Title!"/>
+            </h1>
+            <p>
+                <em>By&nbsp;
+                    <input type="text"
+                           value={username}
+                           className="username-input"
+                           onChange={(event) => {setUsername(event.target.value)}}
+                           placeholder="Enter your username!"/>
+                    </em>
+            </p>
+            <p placeholder="Share your story!">
+                <textarea
+                  value={content}
+                  className="content-input"
+                  onChange={(event) => {setContent(event.target.value)}}
+                  placeholder="Share your story!"/>
+            </p>
+            <button className="share-input" onClick={sharePost}>Share!</button>
+        </div>
+    );
+
+}
 export default Posts;
